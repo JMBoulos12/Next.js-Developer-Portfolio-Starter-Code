074e45a57c8b2e4f788eba848967371a073037c6
Jean Marc Boulos <jmboulos@outlook.com>
2024-08-16 23:27:19 +0300

    Added designed homepage, Hire Me Circular Text and About page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -11,12 +11,9 @@ const Footer = () => {
             <span>{new Date().getFullYear()} &copy; All Rights Reserved!</span>
             <div className="flex items-center">
             Build with<span className='text-primary text-2x1 px-1'>&#9825;</span>
-            by&nbsp;<Link href="/">JMB</Link>
+            by&nbsp;<Link href="https://jmboulos.com" className='underline underline-offset-2' target="_blank">JMB</Link>
             </div>
-            
-            <Link href="https://jmboulos.com" className='underline
-            underline-offset-2
-            '>Say Hello</Link>
+            <Link href="https://jmboulos.com" className='underline underline-offset-2' target="_blank">Say Hello</Link>
 
         </Layout>
     </footer>
---
+++ src/pages/about.js
@@ -0,0 +1,103 @@
+import AnimatedText from '@/components/AnimatedText'
+import Layout from '@/components/Layout'
+import { useInView, useMotionValue, useSpring } from 'framer-motion'
+import Head from 'next/head'
+import Image from 'next/image'
+import React, {useEffect, useRef } from 'react'
+import profilePic from "../../public/images/profile/developer-pic-2.png";
+import { useEffect } from 'react/cjs/react.production.min'
+
+
+const AnimatedNumbers = ({value}) => {
+  const ref = useRef(null);
+  
+const motionValue = useMotionValue(0);  
+const springValue = useSpring(motionValue, { duration: 3000 })
+const isInView = useInView(ref);
+
+useEffect(() => {
+  if (isInView) {
+    motionValue.set(value);
+  }
+}, [isInView, value, motionValue])
+
+useEffect(() => {
+  springValue.on("change", (latest) => {
+
+  })
+
+
+}, [])
+
+
+
+    return <span ref={ref}></span>
+}
+
+
+const about = () => {
+  return (
+    <>
+        <Head>
+            <title>JMB | About Page</title>
+            <meta name="description" content="any description" />
+        </Head>
+      <main className='flex w-full flex-col items-center justify-center'>
+        <Layout className='pt-16'>
+          <AnimatedText text="Passion Fuels Purpose!" className='mb-16'/>
+          <div className='grid w-full grid-cols-8 gap-16'>
+              <div className='col-span-3 flex flex-col items-start justify-start'>
+                <h2 className='mb-4 text-lg font-bold uppercase text-dark/75'>Biography</h2>
+                <p className='font-medium'>
+                I'm Jean Marc BOULOS, a full stack and web developer with a strong passion for designing beautiful, functional, and user-focused digital experiences. With over 11 years of experience, I'm always eager to explore new and creative ways to bring my clients' ideas to life.
+                </p>
+
+                <p className='my-4 font-medium'>
+                I see design as more than just making things visually appealing—it's about addressing challenges and creating seamless, enjoyable experiences for users.
+                </p>
+
+                <p className='font-medium'>
+                Whether I'm developing a website, mobile app, or any digital product, I approach every project with a commitment to design excellence and a focus on user-centered thinking. I'm excited about the possibility of bringing my skills and passion to your next project.
+                </p>
+              </div>
+
+
+
+          <div className='col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
+          bg-light p-8
+          '>
+          <div className='absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark'  />
+              <Image src={profilePic} alt="JMB" className='w-full h-auto rounded-2x1' />
+          </div>
+
+    <div className='col-span-2 flex-col items-end justify-between'>
+
+              <div className='flex flex-col items-end justify-center'>
+                <span className='inline-block text-7xl font-bold'>
+                  50+
+                </span>
+                <h2 className='text-xl font-medium capitalize text-dark/75'>satisfied clients</h2>
+              </div>
+          
+              <div className='flex flex-col items-end justify-center'>
+                <span className='inline-block text-7xl font-bold'>
+                  40+
+                </span>
+                <h2 className='text-xl font-medium capitalize text-dark/75'>projects completed</h2>
+              </div>
+
+              <div className='flex flex-col items-end justify-center'>
+                <span className='inline-block text-7xl font-bold'>
+                  11+
+                </span>
+                <h2 className='text-xl font-medium capitalize text-dark/75'>years of experience</h2>
+              </div>
+            </div>
+          </div>
+        </Layout>
+      </main>
+    </>
+  )
+}
+
+export default about
